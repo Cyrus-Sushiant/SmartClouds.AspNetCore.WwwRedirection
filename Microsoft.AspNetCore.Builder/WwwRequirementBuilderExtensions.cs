c091d4c9fac6fd91a059b674b27f509c60c3b49d
Cyrus <cyrus.sushiant@hotmail.com>
2020-01-17 21:51:17 +0330

    Add project files.

---
+++ Microsoft.AspNetCore.Builder/WwwRequirementBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Rewrite;
+using System;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    /// <summary>
+    /// Extension methods for the WwwRedirection middleware.
+    /// </summary>
+    public static class WwwRequirementBuilderExtensions
+    {
+        /// <summary>
+        /// Adds middleware for redirecting with out WWW Requests to WWW.
+        /// </summary>
+        /// <param name="app">The <see cref="IApplicationBuilder"/> instance this method extends.</param>
+        /// <returns>The <see cref="IApplicationBuilder"/> for HttpsRedirection.</returns>
+        public static IApplicationBuilder UseWwwRedirection(this IApplicationBuilder app)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            var options = new RewriteOptions();
+            options.AddRedirectToWww();
+            app.UseRewriter(options);
+            return app;
+        }
+    }
+}
